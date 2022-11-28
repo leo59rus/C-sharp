aaf9065da59c6ca0eadf6ef60cdf622023fa227f
leonid <orange59usa@gmail.com>
2022-11-28 10:14:08 +0300

    проверка кто назвался для приветсвия

---
+++ examples_5_приветствие любимчика/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите свое имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "леонид") //tolower для ввода в любом регистре инахождения равенства символов
+    {
+    Console.WriteLine("ооу здорова Леонид");
+    }
+else
+    {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+    }
