81452e02f85e8fa3679e64ae3dcf44e7ed801268
leonid <orange59usa@gmail.com>
2022-11-28 11:05:57 +0300

    фрактальный рисунок треугольников

--- examples_7_magic/Program.cs
+++ examples_7_magic/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿Console.Clear();
+
+
+int xa = 40, ya = 1, //вправо 40 вниз 1
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("A");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("B");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("C");
+
+
+int x = xa, y = xb;
+int count = 0;
+while(count < 10000)
+{
+    int what = new Random().Next(0, 3); //ot 0 do 2
+    if(what == 0)
+    {
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count ++; //+1
+}
