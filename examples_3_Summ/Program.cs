f48cada6e05202be39a609e9a5a77e4efdf2a633
leonid <orange59usa@gmail.com>
2022-11-28 09:32:19 +0300

    нахождение суммы

---
+++ examples_3_Summ/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+//второй способо чз ввод промежуточной переменной
+int numberC = 8;
+int numberD = 2;
+int result = numberC + numberD;
+Console.WriteLine(result);
